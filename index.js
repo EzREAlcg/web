9c2fefc5cc0483a12a54bb82022efaa8fced3397
ezrealcg <1414791322@qq.com>
2016-09-03 18:01:48 +0800

    CHENGUO

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+
 document.querySelector('button').addEventListener('click', function(e) {
     document.body.style.color = 'red'
     // 动态添加元素到页面
     var p = document.createElement('p')
     p.innerHTML = '我是新加入的'
     document.body.appendChild(p)
+    console.log("我只能在控制台看到")
+    throw '出错了，但是只能控制台看到'
 })
