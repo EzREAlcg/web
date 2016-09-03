3626b8516e7f9c2046e67cc1ca125afa0190ba1f
ezrealcg <1414791322@qq.com>
2016-09-03 17:05:41 +0800

    我改了一些东西

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 document.querySelector('button').addEventListener('click', function(e) {
     document.body.style.color = 'red'
-    alert('满分')
+    // 动态添加元素到页面
+    var p = document.createElement('p')
+    p.innerHTML = '我是新加入的'
+    document.body.appendChild(p)
 })
